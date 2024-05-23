6ff73a4659ed4dc2e6a50536e400eda02d78b85e
Hala Mansour Ali <halamansour888222555@gmail.com>
2024-05-23 12:40:22 +0300

    Add product for Admin Dashboard done

--- src/app/Components/admin/add-product/add-product.component.ts
+++ src/app/Components/admin/add-product/add-product.component.ts
@@ -1,12 +1,34 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.css'
+  styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
+  product = {
+    name: '',
+    price: 0,
+    description: '',
+    quantity: 0,
+    image: ''
+  };
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    this.http.post('https://localhost:7158/api/Product', this.product)
+      .subscribe({
+        next: (response) => {
+          console.log('Product added successfully', response);
+          this.router.navigate(['/admin/all-products']);
+        },
+        error: (error) => console.error('There was an error!', error)
+      });
+  }
 }
